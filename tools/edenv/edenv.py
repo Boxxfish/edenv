15c520a0501e83cb2b52e97dd605679282c0a423
Boxfish <metakirby123@gmail.com>
2020-04-30 21:24:19 -0400

    Implement init command in edenv.py

--- tools/edenv/edenv.py
+++ tools/edenv/edenv.py
@@ -4,15 +4,28 @@ Main CLI app for EDEnv.
 @author Ben Giacalone
 """
 import click
+from pathlib import Path
+from shutil import copytree
+from os import path
 
 @click.group()
 def main():
     pass
 
-
 @main.command()
-def init():
-    click.echo("init")
+@click.argument("name")
+def init(name):
+    click.echo(f"Creating project \"{name}\"...")
+
+    # Copy project folder from template directory
+    project_path = Path(name)
+    source_path = Path(path.realpath(__file__)).parent.parent.parent / "project"
+    if project_path.is_dir():
+        click.echo(f"Error: Could not create directory \"{name}\".", err=True)
+        return
+    copytree(source_path, project_path)
+
+    click.echo("Done.")
 
 
 @main.command()
