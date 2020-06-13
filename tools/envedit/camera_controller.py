e755a45685e39739f874a8d42a89c02979c98a5a
Boxfish <metakirby123@gmail.com>
2020-06-13 19:15:30 -0400

    Add translation to camera using shift-scroll

--- tools/envedit/camera_controller.py
+++ tools/envedit/camera_controller.py
@@ -6,10 +6,13 @@ Controls the camera.
 from os import path
 from pathlib import Path
 
+import numpy as np
 from direct.showbase import DirectObject
 from direct.task import Task
 from panda3d.core import LVector3f, OrthographicLens, NodePath, ShaderAttrib, Shader, Filename
 import math
+
+from tools.envedit import helper
 
 
 class CameraController(DirectObject.DirectObject):
@@ -28,6 +31,11 @@ class CameraController(DirectObject.DirectObject):
         self.cam_active = True
         self.delta_mouse = (0, 0)
         self.last_mouse = (0, 0)
+        self.start_mouse = (0, 0)
+
+        self.orbit_center = LVector3f(0, 0, 0)
+        self.start_orbit = self.orbit_center
+        self.plane_normal = np.array([0, 0, 0])
 
         # Load skinned shader
         self.base.cam.node().setTagStateKey("shader type")
@@ -54,10 +62,10 @@ class CameraController(DirectObject.DirectObject):
         # Register events
         self.accept("mouse2", self.handle_scroll_pressed)
         self.accept("mouse2-up", self.handle_scroll_released)
+        self.accept("shift-mouse2", self.handle_shift_scroll_pressed)
+        self.accept("shift-mouse2-up", self.handle_shift_scroll_released)
         self.accept("wheel_up", self.handle_scroll_up)
         self.accept("wheel_down", self.handle_scroll_down)
-        self.accept("lshift", self.handle_shift_pressed)
-        self.accept("lshift-up", self.handle_shift_released)
 
         # Set up camera
         self.cam_node.setPos(0, -1, 0)
@@ -74,6 +82,22 @@ class CameraController(DirectObject.DirectObject):
     # Scroll wheel button released handler
     def handle_scroll_released(self):
         self.scroll_down = False
+        self.shift_down = False
+
+    # Scroll wheel button shift pressed handler
+    def handle_shift_scroll_pressed(self):
+        if self.cam_active:
+            self.start_orbit = self.orbit_center
+            self.plane_normal = -self.forward
+            if self.base.mouseWatcherNode.hasMouse():
+                self.start_mouse = (self.base.mouseWatcherNode.getMouseX(), self.base.mouseWatcherNode.getMouseY())
+            self.scroll_down = True
+            self.shift_down = True
+
+    # Scroll wheel button shift released handler
+    def handle_shift_scroll_released(self):
+        self.scroll_down = False
+        self.shift_down = False
 
     # Scroll wheel up handler
     def handle_scroll_up(self):
@@ -95,14 +119,6 @@ class CameraController(DirectObject.DirectObject):
                 # Update camera position in shader
                 self.base.render.set_shader_input("cam_pos", self.cam_node.getPos())
 
-    # Shift button pressed handler
-    def handle_shift_pressed(self):
-        self.shift_down = True
-
-    # Shift button released handler
-    def handle_shift_released(self):
-        self.shift_down = False
-
     # Camera control task
     def cam_ctrl_task(self, task):
         # Update delta mouse
@@ -113,10 +129,14 @@ class CameraController(DirectObject.DirectObject):
         self.last_mouse = curr_mouse
 
         if self.scroll_down:
-            # Translate if shift is down
+            # Translate camera if shift is down
             if self.shift_down:
-                # TODO: Implement this
-                pass
+                start_point = self.get_ray_plane_intersection(self.start_mouse, np.array([0, 0, 0]),self.plane_normal)
+                plane_point = self.get_ray_plane_intersection(curr_mouse, np.array([0, 0, 0]), self.plane_normal)
+                self.orbit_center = self.start_orbit - helper.np_vec3_to_panda(plane_point - start_point)
+
+                # Recalculate camera position and direction
+                self.update_cam()
             # Otherwise, just rotate
             else:
                 # Adjust angles
@@ -140,14 +160,14 @@ class CameraController(DirectObject.DirectObject):
 
     # Recalculates camera position and direction from spherical coords
     def update_cam(self):
-        cam_dist = self.cam_node.getPos().length()
+        cam_dist = (self.orbit_center - self.cam_node.getPos()).length()
         new_x = math.sin(self.up_angle) * math.cos(self.left_angle)
         new_y = math.sin(self.up_angle) * math.sin(self.left_angle)
         new_z = math.cos(self.up_angle)
-        new_pos = LVector3f(new_x, new_y, new_z) * cam_dist
+        new_pos = self.orbit_center + LVector3f(new_x, new_y, new_z) * cam_dist
         self.cam_node.setPos(new_pos)
-        self.forward = (-self.cam_node.getPos()).normalized()
-        self.cam_node.lookAt(0, 0, 0)
+        self.forward = (self.orbit_center - self.cam_node.getPos()).normalized()
+        self.cam_node.lookAt(self.orbit_center)
 
         # Update camera position in shader
         self.base.render.set_shader_input("cam_pos", new_pos)
@@ -155,3 +175,30 @@ class CameraController(DirectObject.DirectObject):
     # "Destructor" for camera controller
     def destroy(self):
         self.ignore_all()
+
+    # Returns the world space coordinate of a view space point
+    # The world space coordinate is projected onto a 2D plane facing the camera
+    def view_to_world(self, view_point):
+        camera = self.base.camera
+        proj_mat = helper.panda_mat4_to_np(self.base.camLens.getProjectionMat())
+        cam_mat = np.linalg.inv(helper.panda_mat4_to_np(camera.getTransform().getMat()))
+        view_to_world_mat = np.linalg.inv(cam_mat).dot(np.linalg.inv(proj_mat))
+        world_point = view_to_world_mat.dot(np.array([view_point[0], view_point[1], -1, 1]))[:3]
+        return world_point
+
+    # Returns the world space intersection of a camera ray and a plane
+    # The ray is generated from a view space coordinate
+    # The plane is in world space
+    def get_ray_plane_intersection(self, view_point, plane_center, plane_normal):
+        # Convert screen point to ray in world space
+        cam_pos = self.base.camera.getTransform().getPos()
+        ray_origin = np.array([cam_pos[0], cam_pos[1], cam_pos[2]])
+        ray_dir = self.view_to_world(view_point) - ray_origin
+
+        # Calculate intersection
+        denom = ray_dir.dot(plane_normal)
+        if abs(denom) < 0.001:
+            return None
+        else:
+            multiplier = (plane_center - ray_origin).dot(plane_normal) / denom
+            return ray_origin + ray_dir * multiplier
